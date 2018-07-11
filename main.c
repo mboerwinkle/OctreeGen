2b98de9cae39b558bb47ee5f95599bfef19aa140
mboerwinkle <mboerwinkle@gmail.com>
2018-07-11 14:34:31 -0500

    added command line resolution option to avoid recompiling

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "structures.h"
 
 extern void processFile(FILE* inputFile, FILE* outputFile);
-
 int main(int argc, char** argv){
 	FILE *inputFile, *outputFile;
 	if(argc == 1){//process everything
@@ -27,20 +27,24 @@ int main(int argc, char** argv){
 			outputFile = fopen(extFilename, "w");
 			processFile(inputFile, outputFile);
 		}
-	}else if(argc >= 2 && argc < 4){//process only indicated file
+	}else if(argc >= 2 && argc < 5){//process only indicated file
 		inputFile = fopen(argv[1], "r");
 		if(inputFile == NULL){
 			printf("failed to open %s\n", argv[1]);
 			return 1;
 		}
-		if(argc == 3){//if a second argument is given, it is the output file
+		if(argc > 2){//if a second argument is given, it is the output file
 			outputFile = fopen(argv[2], "w");
+			if(argc > 3){
+				resolution = atof(argv[3]);
+				printf("Resolution set to %lf\n", resolution);
+			}
 		}else{
 			outputFile = stdout;
 		}
 		processFile(inputFile, outputFile);
 	}else{//usage
-		puts("Usage: ./run [input file [output file]]");
+		puts("Usage: ./run [input file [output file [resolution]]]");
 		return 1;
 	}
 	return 0;
