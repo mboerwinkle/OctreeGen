3714bfb0feb25665059f544d86a78d97bb59f039
mboerwinkle <mboerwinkle@gmail.com>
2018-07-15 18:17:45 -0500

    fixed the variable level flood-fill

--- findContiguousOctree.c
+++ findContiguousOctree.c
@@ -44,6 +44,10 @@ void dequeue(int *x, int *y, int *z, int *mag, int *dir){
 	fDequeue = fDequeue->next;
 	free(old);
 }
+void enqueueAndFrontier(oct* f, int x, int y, int z, int mag, int dir){//FIXME nasty as fuck
+	addCorner(f, x, y, z, mag);
+	enqueue(x, y, z, mag, dir);
+}
 
 oct* findContiguousOctree(oct* t, int x, int y, int z){
 	puts("findContiguousOctree called");
@@ -62,18 +66,17 @@ oct* findContiguousOctree(oct* t, int x, int y, int z){
 	f->full = 0;
 	f->mag = t->mag;
 	memset(f->child, 0, 8*sizeof(oct*));
-	addCorner(f, x, y, z, 0);
-	enqueue(x, y, z, 0, 0);//FIXME would it be better to start with a high magnitude?
+	enqueueAndFrontier(f, x, y, z, 0, 0);
 	long int totalSize = 0;
 	//frontier expansion loop
 	while(fDequeue != NULL){//while there are still unexplored frontiers
 		int fx, fy, fz, mag, dir;
 		dequeue(&fx, &fy, &fz, &mag, &dir);
-		printf("dequeued %d: %d %d %d mag: %d dir: %d\n", queueLen, fx, fy, fz, mag, dir);
+//		printf("dequeued %d: %d %d %d mag: %d dir: %d\n", queueLen, fx, fy, fz, mag, dir);
 		int foundMag;
 		int cornerExistsRet = cornerExists(t, fx, fy, fz, mag, &foundMag);
 		if(cornerExistsRet == 2){//exists fully
-			printf("full at lvl %d\n", foundMag);
+//			printf("full at lvl %d\n", foundMag);
 			int sideLen = 1<<foundMag;
 			if(foundMag > mag){
 				addCorner(f, fx, fy, fz, foundMag);
@@ -82,123 +85,74 @@ oct* findContiguousOctree(oct* t, int x, int y, int z){
 			//totalSize++;//FIXME there is an error in the total size calculation. it is too big. check if addCorner is adding spots that have already been added?
 			int fCornerExists = cornerExists(f, fx-sideLen, fy, fz, foundMag, NULL);
 			if(fCornerExists == 0/* || fCornerExists == 1*/){
-				addCorner(f, fx-sideLen, fy, fz, foundMag);
-				enqueue(fx-sideLen, fy, fz, foundMag, 0);
+				enqueueAndFrontier(f, fx-sideLen, fy, fz, foundMag, 0);
 			}
 			fCornerExists = cornerExists(f, fx+sideLen, fy, fz, foundMag, NULL);
 			if(fCornerExists == 0/* || fCornerExists == 1*/){
-				addCorner(f, fx+sideLen, fy, fz, foundMag);
-				enqueue(fx+sideLen, fy, fz, foundMag, 1);
+				enqueueAndFrontier(f, fx+sideLen, fy, fz, foundMag, 1);
 			}
 			fCornerExists = cornerExists(f, fx, fy-sideLen, fz, foundMag, NULL);
 			if(fCornerExists == 0/* || fCornerExists == 1*/){
-				addCorner(f, fx, fy-sideLen, fz, foundMag);
-				enqueue(fx, fy-sideLen, fz, foundMag, 2);
+				enqueueAndFrontier(f, fx, fy-sideLen, fz, foundMag, 2);
 			}
 			fCornerExists = cornerExists(f, fx, fy+sideLen, fz, foundMag, NULL);
 			if(fCornerExists == 0/* || fCornerExists == 1*/){
-				addCorner(f, fx, fy+sideLen, fz, foundMag);
-				enqueue(fx, fy+sideLen, fz, foundMag, 3);
+				enqueueAndFrontier(f, fx, fy+sideLen, fz, foundMag, 3);
 			}
 			fCornerExists = cornerExists(f, fx, fy, fz-sideLen, foundMag, NULL);
 			if(fCornerExists == 0/* || fCornerExists == 1*/){
-				addCorner(f, fx, fy, fz-sideLen, foundMag);
-				enqueue(fx, fy, fz-sideLen, foundMag, 4);
+				enqueueAndFrontier(f, fx, fy, fz-sideLen, foundMag, 4);
 			}
 			fCornerExists = cornerExists(f, fx, fy, fz+sideLen, foundMag, NULL);
 			if(fCornerExists == 0/* || fCornerExists == 1*/){
-				addCorner(f, fx, fy, fz+sideLen, foundMag);
-				enqueue(fx, fy, fz+sideLen, foundMag, 5);
+				enqueueAndFrontier(f, fx, fy, fz+sideLen, foundMag, 5);
 			}
 		}else if(cornerExistsRet == 1){//FIXME this should delete the subtree, then duplicate the ret tree to the stump. And then add the 4 new frontier full subtrees
-			puts("partial, subdividing");
-			int isFullTest;
-			oct* target = getSubOctree(f, fx, fy, fz, mag, &isFullTest);
-			if(target == NULL){
-				printf("Im about to fail. Null cube full: %d\n", isFullTest);
-				continue;
-			}
-			if(!target->full) puts("this shouldnt happen...");
-			target->full = 0;
+//			puts("partial, subdividing");
+			deleteSubTree(f, fx, fy, fz, mag);
 			int o = 1<<(mag-1);//offset
 			if(dir == 0){//100, 101, 110, 111//FIXME remove code duplication
 				//4, 5, 6, 7
-				addCorner(target, o, 0, 0, mag-1);
-				enqueue(fx+o, fy, fz, mag-1, dir);
-				addCorner(target, o, 0, o, mag-1);
-				enqueue(fx+o, fy, fz+o, mag-1, dir);
-				addCorner(target, o, o, 0, mag-1);
-				enqueue(fx+o, fy+o, fz, mag-1, dir);
-				addCorner(target, o, o, o, mag-1);
-				enqueue(fx+o, fy+o, fz+o, mag-1, dir);
+				enqueueAndFrontier(f, fx+o, fy+0, fz+0, mag-1, dir);
+				enqueueAndFrontier(f, fx+o, fy+0, fz+o, mag-1, dir);
+				enqueueAndFrontier(f, fx+o, fy+o, fz+0, mag-1, dir);
+				enqueueAndFrontier(f, fx+o, fy+o, fz+o, mag-1, dir);
 			}
 			if(dir == 1){//000, 001, 010, 011
 				//0, 1, 2, 3
-				addCorner(target, 0, 0, 0, mag-1);
-				enqueue(fx, fy, fz, mag-1, dir);
-				addCorner(target, 0, 0, o, mag-1);
-				enqueue(fx, fy, fz+o, mag-1, dir);
-				addCorner(target, 0, o, 0, mag-1);
-				enqueue(fx, fy+o, fz, mag-1, dir);
-				addCorner(target, 0, o, o, mag-1);
-				enqueue(fx, fy+o, fz+o, mag-1, dir);
+				enqueueAndFrontier(f, fx+0, fy+0, fz+0, mag-1, dir);
+				enqueueAndFrontier(f, fx+0, fy+0, fz+o, mag-1, dir);
+				enqueueAndFrontier(f, fx+0, fy+o, fz+0, mag-1, dir);
+				enqueueAndFrontier(f, fx+0, fy+o, fz+o, mag-1, dir);
 			}
 			if(dir == 2){//010, 011, 110, 111
 				//2, 3, 6, 7
-				addCorner(target, 0, o, 0, mag-1);
-				enqueue(fx, fy+o, fz, mag-1, dir);
-				addCorner(target, 0, o, o, mag-1);
-				enqueue(fx, fy+o, fz+o, mag-1, dir);
-				addCorner(target, o, o, 0, mag-1);
-				enqueue(fx+o, fy+o, fz, mag-1, dir);
-				addCorner(target, o, o, o, mag-1);
-				enqueue(fx+o, fy+o, fz+o, mag-1, dir);
+				enqueueAndFrontier(f, fx+0, fy+o, fz+0, mag-1, dir);
+				enqueueAndFrontier(f, fx+0, fy+o, fz+o, mag-1, dir);
+				enqueueAndFrontier(f, fx+o, fy+o, fz+0, mag-1, dir);
+				enqueueAndFrontier(f, fx+o, fy+o, fz+o, mag-1, dir);
 			}
 			if(dir == 3){//000, 001, 100, 101
 				//0, 1, 4, 5
-				addCorner(target, 0, 0, 0, mag-1);
-				enqueue(fx, fy, fz, mag-1, dir);
-				addCorner(target, 0, 0, o, mag-1);
-				enqueue(fx, fy, fz+o, mag-1, dir);
-				addCorner(target, o, 0, 0, mag-1);
-				enqueue(fx+o, fy, fz, mag-1, dir);
-				addCorner(target, o, 0, o, mag-1);
-				enqueue(fx+o, fy, fz+o, mag-1, dir);
+				enqueueAndFrontier(f, fx+0, fy+0, fz+0, mag-1, dir);
+				enqueueAndFrontier(f, fx+0, fy+0, fz+o, mag-1, dir);
+				enqueueAndFrontier(f, fx+o, fy+0, fz+0, mag-1, dir);
+				enqueueAndFrontier(f, fx+o, fy+0, fz+o, mag-1, dir);
 			}
 			if(dir == 4){//001, 011, 101, 111
 				//1, 3, 5, 7
-				addCorner(target, 0, 0, o, mag-1);
-				enqueue(fx, fy, fz+o, mag-1, dir);
-				addCorner(target, 0, o, o, mag-1);
-				enqueue(fx, fy+o, fz+o, mag-1, dir);
-				addCorner(target, o, 0, o, mag-1);
-				enqueue(fx+o, fy, fz+o, mag-1, dir);
-				addCorner(target, o, o, o, mag-1);
-				enqueue(fx+o, fy+o, fz+o, mag-1, dir);
+				enqueueAndFrontier(f, fx+0, fy+0, fz+o, mag-1, dir);
+				enqueueAndFrontier(f, fx+0, fy+o, fz+o, mag-1, dir);
+				enqueueAndFrontier(f, fx+o, fy+0, fz+o, mag-1, dir);
+				enqueueAndFrontier(f, fx+o, fy+o, fz+o, mag-1, dir);
 			}
 			if(dir == 5){//000, 010, 100, 110
 				//0, 2, 4, 6
-				addCorner(target, 0, 0, 0, mag-1);
-				enqueue(fx, fy, fz, mag-1, dir);
-				addCorner(target, 0, o, 0, mag-1);
-				enqueue(fx, fy+o, fz, mag-1, dir);
-				addCorner(target, o, 0, 0, mag-1);
-				enqueue(fx+o, fy, fz, mag-1, dir);
-				addCorner(target, o, o, 0, mag-1);
-				enqueue(fx+o, fy+o, fz, mag-1, dir);
+				enqueueAndFrontier(f, fx+0, fy+0, fz+0, mag-1, dir);
+				enqueueAndFrontier(f, fx+0, fy+o, fz+0, mag-1, dir);
+				enqueueAndFrontier(f, fx+o, fy+0, fz+0, mag-1, dir);
+				enqueueAndFrontier(f, fx+o, fy+o, fz+0, mag-1, dir);
 			}
-
-			//FIXME is from here:
-/*			int full;
-			oct* retDuplicate = getSubOctree(ret, fx, fy, fz, mag, &full);
-			if(retDuplicate == NULL){
-				if(full){
-					//FIXME fill in the frontier and warn that this shouldn't happen
-				}
-			}else{
-				unionOctree(target, retDuplicate);
-			}*/
-			//to here necessary or not?
 		}
 //		printOctree(f);
 //		getchar();
