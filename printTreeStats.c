8b31da609a14fafc02beb0fd59c28f631fdf4ecb
Martin Boerwinkle <mboerwinkle@gmail.com>
2021-06-15 12:00:11 -0400

    added total volume printing

--- printTreeStats.c
+++ printTreeStats.c
@@ -4,26 +4,29 @@
 #include "octreeOps.h"
 
 int* cubeCount = NULL;
-void printTreeStats(oct* t){
-	int topLevel = 0;
-	if(cubeCount == NULL){
-		cubeCount = calloc(t->mag+1, sizeof(int));
-		topLevel = 1;
-	}
+unsigned long int cubeVolume = 0;
+
+void printTreeStatsRec(oct* t){
 	if(t->full){
 		(cubeCount[t->mag])++;
+		int sidelen = 1<<t->mag;
+		cubeVolume += sidelen * sidelen * sidelen;
 	}else{
 		for(int c = 0; c < 8; c++){
 			if(t->child[c] != NULL){
-				printTreeStats(t->child[c]);
+				printTreeStatsRec(t->child[c]);
 			}
 		}
 	}
-	if(topLevel){
-		puts("CUBE BREAKDOWN");
-		for(int x = t->mag; x >= 0; x--){
-			printf("%d: %d\n", x, cubeCount[x]);
-		}
-		free(cubeCount);
+}
+void printTreeStats(oct* t){
+	cubeCount = calloc(t->mag+1, sizeof(int));
+	printTreeStatsRec(t);
+	printf("CUBE VOLUME: %ld\n", cubeVolume);
+	puts("CUBE BREAKDOWN");
+	for(int x = t->mag; x >= 0; x--){
+		printf("%d: %d\n", x, cubeCount[x]);
 	}
+	free(cubeCount);
+	cubeCount = NULL;
 }
